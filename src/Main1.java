26288e3da540c6d05a69e38bb6c43ecfb9443277
Brahim El Manti <81900171+belmanti@users.noreply.github.com>
2022-01-30 21:49:34 +0100

    Step2

--- src/Main1.java
+++ src/Main1.java
@@ -4,11 +4,15 @@ public class Main1 {
 	public static void main(String[] args) {
 		System.out.println("Hello! world");
 		System.out.println(Somme(1,2));
+		System.out.println(Somme(3,2));
 		
 
 	}
 	public static int Somme(int a , int b ){
 		return a+b;
+	}
+	public static int multiplication(int a , int b ){
+		return a*b;
 	}
 
 }
