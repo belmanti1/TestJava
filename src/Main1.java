e6230c358d24aa2e04895f254230d13804a1bf14
Brahim El Manti <wiesbaden.hessen.Deutsh@gmail.Com>
2022-01-30 21:18:06 +0100

    step1

--- src/Main1.java
+++ src/Main1.java
@@ -3,7 +3,12 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello! world");
+		System.out.println(Somme(1,2));
+		
 
+	}
+	public static int Somme(int a , int b ){
+		return a+b;
 	}
 
 }
